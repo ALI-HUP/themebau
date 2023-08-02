043d67ef9f875cf917867213a923a790e31be532
WORK SPACE <ali.sheikhbahaie84@gmail.com>
2023-08-02 17:29:54 +0330

    themebau

---
+++ Themebau.js
@@ -0,0 +1,37 @@
+'use strict'
+
+
+// var num = "55";
+// console.log(num);
+
+
+// var a=5;
+// var b=2.2;
+// console.log(a/b);
+
+// if(false) {
+//     console.log('hello');
+// } else {
+//     console.log('bye');
+// }
+
+// for (var i = 0; i < 101 ; i++) {
+//     console.log(i)
+// }
+
+
+
+// var car = {
+//     make : 'Ford',
+//     model : 'must',
+//     year : 1400,
+//     details : {
+//         color : 'red',
+//         whiles : '3',
+//     }
+// }
+// car.name = "fordmustang";
+
+
+// console.log(car)
+
